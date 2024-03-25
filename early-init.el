08b5f828164601d038eb0e4b010106b83ae40aa8
yx <yangxue.cs@foxmail.com>
2024-03-25 21:09:32 +0800

    typo

--- early-init.el
+++ early-init.el
@@ -15,8 +15,8 @@
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars . 0) default-frame-alist)
-(push '(undecorated-round . t)  default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(undecorated-round . t) default-frame-alist)
 (push '(fullscreen . maximized) initial-frame-alist)
 (when (featurep 'ns) (push '(ns-transparent-titlebar . t) default-frame-alist))
 
